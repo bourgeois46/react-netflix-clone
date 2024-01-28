832302298876e065933b297d331fc0e27369c420
bourgeois <juahsome2795@gmail.com>
2024-01-28 17:48:46 +0900

    first commit

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params:{
+        api_key: "34039c63967aacbc2429413a1757fbcf",
+        language: "ko-KR",
+    }
+});
+
+export default instance;
